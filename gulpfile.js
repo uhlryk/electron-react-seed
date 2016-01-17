e2349161206b3e84c8c2e7afdf165be5758c38c6
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2016-01-17 21:12:17 +0100

    gulp prebuild task, npm prebuild script

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell')
 
 gulp.task('copy-electron', function() {
   return gulp.src(['./src/main.js', './src/index.html'])
     .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('run-electron-prebuild', shell.task('./node_modules/.bin/electron ./dist/main'));
