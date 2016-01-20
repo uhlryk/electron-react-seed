a96814dbf28e692e8227bc4ceefd29e35c539d71
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2016-01-21 00:11:01 +0100

    import scss styles to index.jsx

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import style from './sass/style.scss';
 import createStore from './stores/index.js';
 import reducer from './reducers/index.js';
 import App from './components/App.jsx';
