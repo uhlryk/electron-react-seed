729017e71b384ff5bec4eba5c7d3015af9bc73dd
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2018-02-11 17:33:42 +0100

    move frontend logic from html file

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class App extends React.Component {
+export default class Root extends React.Component {
   render() {
     return (<div>
       <h2>Welcome to React!</h2>
