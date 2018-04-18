a58e7b56c22d7a9331cd1a851e24b4aeaddf25dd
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2018-04-18 22:19:12 +0200

    fix router

--- src/frontend/modules/tasks/index.jsx
+++ src/frontend/modules/tasks/index.jsx
@@ -1,5 +1,5 @@
-import React, { Fragment }  from "react";
-import { Route, Link, Redirect } from "react-router-dom";
+import React from "react";
+import { Route, Link } from "react-router-dom";
 
 import reducer from "./reducer";
 import epics from "./epics";
@@ -15,26 +15,16 @@ export default function() {
             translation: translation
         },
         events: {
-            onRenderMenuLink: (translations) => <li>
-                <Link to={constants.MODULE_ROUTE_PATH}>
-                    {translations.t(
-                        constants.NAME +
-                        ":" +
-                        constants.NAME
-                    )}
-                </Link>
-            </li>,
-            onRenderRoute: () => <Fragment>
-                <Redirect
-                    exact
-                    from="/"
-                    to={constants.MODULE_ROUTE_PATH}
-                />
-                <Route
-                    path={constants.MODULE_ROUTE_PATH}
-                    component={Main}
-                />
-            </Fragment>
+            onRenderMenuLink: translations => (
+                <li>
+                    <Link to={constants.MODULE_ROUTE_PATH}>
+                        {translations.t(constants.NAME + ":" + constants.NAME)}
+                    </Link>
+                </li>
+            ),
+            onRenderRoute: () => (
+                <Route path={constants.MODULE_ROUTE_PATH} component={Main} />
+            )
         }
     };
 }
